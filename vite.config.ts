774a1c73b28a8b3b227759c620690a1785f244a0
CaptainMentallic <shaanjaswal01@gmail.com>
2025-10-05 10:31:12 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,21 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    server: {
+        host: true,
+        allowedHosts: [
+            'fwcc0wggo0sskws400gc40wg.172.219.50.229.sslip.io',
+            'starry-faustina-unceriferous.ngrok-free.dev'
+        ]
+    },
+
+    preview: {
+        host: true,
+        port: 80,
+        allowedHosts: [
+            'fwcc0wggo0sskws400gc40wg.172.219.50.229.sslip.io',
+            'starry-faustina-unceriferous.ngrok-free.dev'
+        ]
+    },
 });
