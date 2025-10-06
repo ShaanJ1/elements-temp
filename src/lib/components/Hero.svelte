9e780dd0e864f0bc4a5b93d5027c2c9cca3161fc
CaptainMentallic <shaanjaswal01@gmail.com>
2025-10-05 21:33:45 -0600

    Update Hero.svelte

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -14,7 +14,7 @@
 	<div class="relative">
 		<div class="mx-auto max-w-7xl px-4 py-24 sm:px-6 sm:py-32 lg:px-8">
 			<h1 class="text-4xl font-extrabold tracking-tight text-text-inverse sm:text-5xl md:text-6xl">
-				<span class="block">Elements Millwork &</span>
+				<span class="block text-white">Elements Millwork &</span>
 				<span class="block text-primary-light">Flooring Inc.</span>
 			</h1>
 			<p class="mt-6 max-w-lg text-xl text-text-muted sm:max-w-3xl">
